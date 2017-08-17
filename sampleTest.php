a8fb222686c5ca568806fb8d76c299b7cf3cc88c
a-yamazaki <a-yamazaki@advanced-media.co.jp>
2017-08-17 19:01:59 +0900

    Update sampleTest.php

--- sampleTest.php
+++ sampleTest.php
@@ -1,9 +1,7 @@
 <?php
 require_once('./sample.php');
-class SampleTest extends PHPUnit_Framework_TestCase
-{
-    public function test()
-    {
+class SampleTest extends PHPUnit_Framework_TestCase {
+    public function test() {
         $Sample = new Sample();
         $this->assertEquals($Sample->helloWorld(), 'hello world!');
     }
