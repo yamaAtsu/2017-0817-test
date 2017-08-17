61d99e85e757af1d33188f4b440f108c1a7a1aba
a-yamazaki <a-yamazaki@advanced-media.co.jp>
2017-08-17 18:26:14 +0900

    upload_test_code

---
+++ sampleTest.php
@@ -0,0 +1,10 @@
+<?php
+require_once('./sample.php');
+class SampleTest extends PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+        $Sample = new Sample();
+        $this->assertEquals($Sample->helloWorld(), 'hello world!');
+    }
+}
